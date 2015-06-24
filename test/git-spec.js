ad4819c4d27c2498c82223e5085a559e8962ff21
joeyism <sham.joey@gmail.com>
2015-06-24 16:45:25 -0400

    Updated git-lib

--- test/git-spec.js
+++ test/git-spec.js
@@ -183,11 +183,11 @@ describe('git', function(){
         });
 
         it('should correctly display files added', function(done){
-            var result = 'On branch master\nChanges to be committed:\n(use "git reset HEAD <file>..." to unstage)\nmodified:   coverage/lcov-report/index.html\nmodified:   coverage/lcov-report/lib/git.js.html\nmodified:   coverage/lcov-report/lib/index.html\n';
+            var result = 'On branch master\nYour branch is up to date with master\n\nChanges to be committed:\n(use "git reset HEAD <file>..." to unstage)\n\nmodified:   coverage/lcov-report/index.html\nmodified:   coverage/lcov-report/lib/git.js.html\nmodified:   coverage/lcov-report/lib/index.html\n';
             mockery.registerMock('child_process', fakeChild(null, result));
             git = require('../lib/git');
             git.showFilesAdded().then(function(result){
-                expect(result).to.equal('modified:   coverage/lcov-report/lib/git.js.html');
+                expect(result).to.equal('modified:   coverage/lcov-report/index.html\nmodified:   coverage/lcov-report/lib/git.js.html');
                 done();
             });             
         });
